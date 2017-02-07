bf7ce2ba170e98ee6783f7e204b1ba089543cd35
Maya Lekova <apokalyptra@gmail.com>
2017-02-07 10:00:31 +0100

    Initial commit - add Firebase pod

---
+++ IsNowIllegal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IsNowIllegal
+//
+//  Created by Maya Lekova on 2/7/17.
+//  Copyright © 2017 Maya Lekova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'IsNowIllegal' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IsNowIllegal
+  pod 'Firebase', '~> 3.12'
+
+  target 'IsNowIllegalTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'IsNowIllegalUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
